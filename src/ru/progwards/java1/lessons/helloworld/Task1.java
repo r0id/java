07ccd51315f6339396f142593956c1324e62eb22
r0id <r@r.ru>
2019-11-07 14:46:52 +0300

    Revert "New file and public class:WrongClass"
    This reverts commit bf05acb2

---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,20 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        String x = "Хорошо идут дела";
+        String y = "Изучаю Java я!";
+        String z = "";
+
+        System.out.println(x);
+        System.out.print(y);
+        System.out.println(z);
+
+        System.out.println("Хорошо идут дела.");
+        System.out.println("Изучаю Java я!");
+        System.out.println("Хорошо идут дела Изучаю Java я!");
+        System.out.print("Изучаю Java я! Хорошо идут дела.");
+
+    }
+}
