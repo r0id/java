d742c40b0f30f8b517925040ade2ecb49d6d7bca
r0id <r@r.ru>
2019-11-08 09:02:41 +0300

    Task1 i wrote new a code. I'm think to code really the best. Version three.

--- src/ru/progwards/java1/lessons/helloworld/Task1.java
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -1,19 +1,23 @@
 package ru.progwards.java1.lessons.helloworld;
 
 public class Task1 {
-
+    static void Str() {
+        String e;
+        String r;
+        String y;
+        e = "Хорошо идут дела";
+        r = "Изучаю Java я!";
+        y = " ";
+        System.out.println(e);
+        System.out.println(r);
+        System.out.print(e);
+        System.out.print(y);
+        System.out.println(r);
+        System.out.print(r);
+        System.out.print(y);
+        System.out.println(e);
+    }
     public static void main(String[] args) {
-
-        String str;
-        String str1 = "Хорошо идут дела";
-        String str2 = "Изучаю Java я!";
-        String str3 = " ";
-        String str4 = "Хорошо идут дела Изучаю Java я!";
-        String str5 = "Изучаю Java я! Хорошо идут дела.";
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println(str4);
-        System.out.print(str5);
-
+        Str();
     }
 }
